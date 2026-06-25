c9071a2dd9f3e833c0b750c4d138494dcf0bbafb
Fabio Costa <developerfabiocsilva@gmail.com>
2026-06-25 18:17:30 -0300

    fix: adicionando configuração vazia para turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,13 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
- 
-  disable: process.env.NODE_ENV === "development",
-});
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   
-   webpack: (config: any, options: { dev: boolean; isServer: boolean }) => {
-     return config;
-  },
-
+  turbopack: {}, 
   
+  
+  webpack: (config: any, options: { dev: boolean; isServer: boolean }) => {
+    return config;
+  },
 };
 
-
-
-module.exports = nextConfig;
-
-export default withPWA(nextConfig);
+export default nextConfig;
