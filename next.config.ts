741185eee794197d68426b5a8fe7b40d6dba71a9
Fabio Costa <developerfabiocsilva@gmail.com>
2026-06-25 01:41:53 -0300

    fix: corrigindo erros de tipos no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,21 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  // Desativa o PWA no modo de desenvolvimento para não atrapalhar com cache antigo enquanto você programa
+ 
   disable: process.env.NODE_ENV === "development",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Se você tiver outras configurações do Next.js, elas ficam aqui dentro
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  
+  webpack: (config: any, options: { dev: boolean; isServer: boolean }) => {
+    return config;
+  },
 };
+
+
+
+module.exports = nextConfig;
 
 export default withPWA(nextConfig);
